d4a1982460782c19b895d366546c95dc0a25af8e
DavBatts <dsbatts13@gmail.com>
2021-11-19 00:54:37 -0500

    Completes assignment
    Would not push originally.

---
+++ Game.gd
@@ -0,0 +1,12 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var new_dialogue = Dialogic.start('conversation')
+	add_child(new_dialogue)
